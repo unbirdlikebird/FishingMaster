215ea5ec8d486d9f83c62198772b24e0fde74ec0
Eric <unbirdlikebird@hotmail.com>
2015-05-06 08:51:40 +0800

    first push

--- GameVC.h
+++ GameVC.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  GameVC.h
 //  FishingMaster
 //
 //  Created by Henry on 5/6/15.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface GameVC : UIViewController
 
 @end
---
+++ GameVC.m
@@ -0,0 +1,38 @@
+//
+//  GameVC.m
+//  FishingMaster
+//
+//  Created by Henry on 5/6/15.
+//  Copyright (c) 2015 unbirdlikebird. All rights reserved.
+//
+
+#import "GameVC.h"
+
+@interface GameVC ()
+
+@end
+
+@implementation GameVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
